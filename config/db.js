0d246930519b62b1bcb7977e892d327b4bdfafcc
Amarkant Kumar <amar.du2013@gmail.com>
2016-05-02 01:31:22 +0530

    DB changed.

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/expressnode', function(err) {
+
+// mongo ds011912.mlab.com:11912/expressnode -u <dbuser> -p <dbpassword>
+// mongoose.connect('mongodb://localhost:27017/expressnode', function(err) {
+mongoose.connect('mongodb://amar.du2013:amar.du2013@ds011912.mlab.com:11912/expressnode', function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
