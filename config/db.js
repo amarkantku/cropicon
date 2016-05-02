3843cec1ab4d6278b31ed2e3cbb34169dd539731
Amarkant Kumar <amar.du2013@gmail.com>
2016-05-03 00:13:22 +0530

    Mongo DB URI config done

--- config/db.js
+++ config/db.js
@@ -1,8 +1,22 @@
+'use strict';
+
+var express = require('express');
 var mongoose = require('mongoose');
+var app = express();
+
+// development only
+if ('development' === app.get('env')) {
+  app.set('MONGOBD_URI', 'mongodb://localhost:27017/expressnode');
+}
 
 // mongo ds011912.mlab.com:11912/expressnode -u <dbuser> -p <dbpassword>
-// mongodb://localhost:27017/expressnode
-mongoose.connect('mongodb://amar.du2013:amar.du2013@ds011912.mlab.com:11912/expressnode', function(err) {
+// production only
+if ('production' === app.get('env')) {
+  app.set('MONGOBD_URI', 'mongodb://amar.du2013:amar.du2013@ds011912.mlab.com:11912/expressnode');
+}
+
+// connection setup
+mongoose.connect(app.get('MONGOBD_URI'), function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
